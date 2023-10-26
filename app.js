2511cfeacba2080e1edf6d57de3ba817404bb847
victoriabernadskiy <vbernadskiy0531@gmail.com>
2023-10-26 00:12:45 -0400

    ed

--- app.js
+++ app.js
@@ -1,50 +1,22 @@
- const DOMSelectors = {
-    title: document.getElementById("header-title"),
-    summary: document.querySelector(".summary"),
-    newdiv: document.getElementById("newdiv"),
-};
-
-console.log(DOMSelectors.title)
-
-DOMSelectors.title.innerText = ("Sleepy Time")
-
-function appendTitle(extraword) {
-    DOMSelectors.title.innerText = DOMSelectors.title.innerText + extraword;
+const DOMSelectors = {
+    card_holder: document.getElementById("card_holder"),
+    card_form: document.getElementById("card_form"),
+    name_input: document.getElementById("name_input"),
+    age_input: document.getElementById("age_input"),
+    city_input: document.getElementById("city_input"),
+    btn: document.getElementById("btn"),
 }
 
-appendTitle ("poopystinky");
+DOMSelectors.card_form.addEventListener("submit",(event) => {
+    event.preventDefault()
+    const new_card = {
+        name: DOMSelectors.name_input.value,
+        age: DOMSelectors.age_input.value,
+        city: DOMSelectors.city_input.value
+    }
+    add_card(new_card)
+})
 
-console.log(DOMSelectors.newdiv.children[0].innerText); 
-
- function displayName() {
-    var originalName = document.getElementById("txtInputData").value;
-    document.getElementById("show_name").innerHTML = "Your Name is " + originalName;
- } 
- //insert adjacent html
-
- const DOMSelectors = {
-    Name: document.getElementById("input 1"),
-    Age: document.getElementById("input 2"),
-    City: document.getElementById("input 3"),
-    cardButton: document.getElementById("btn"),
- }
-function makeAlbum(){
-
+function add_card(card){
+    console.log(card)
 }
-
-function addCard(Album)
-
-DOMSelectors.cardButton.addEventListener("create card", function (event){
-event.preventDefault();
-// const Album = makeAlbum()
-//addCard()
-// clearFields()
-//addRemoveButtons
-document
-.querySelector()
-.insertAdjacentHTML()
-    })
-
-    btn.addEventListener('click', function (e) {
-        console.log(e.target);
-      });
